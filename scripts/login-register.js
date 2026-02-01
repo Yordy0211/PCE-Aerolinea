d2a74a7e29948a249f00f89ce62cbe017fb8cf61
Yordy Ventura <venturayordy19@gmail.com>
2026-02-01 11:48:45 -0400

    funcion para crear un usuario y guardarlo en local

--- scripts/login-register.js
+++ scripts/login-register.js
@@ -1,18 +1,60 @@
-const goRegister = document.getElementById("go-register")
+//Esto hace que al hacer click en registrarse cambiede de iniciar sesion-
+//a registrarse y al revéz
+
+const goRegister = document.getElementById("go-register") //esto toma el link de registrarse y login y lo guarda en esta variable
 const goLogin = document.getElementById("go-login")
 
-const loginForm = document.getElementById("login-box")
-const registerForm = document.getElementById("register-box")
+const loginContainer = document.getElementById("login-box")//esto busca el contenedordel login y register en el archivo html
+const registerContainer = document.getElementById("register-box")
 
-goRegister.addEventListener("click", function (e) {
+goRegister.addEventListener("click", function (e) { //esta funcion hace que al hacer click en registrarse cambie al form de registro
     e.preventDefault()
-    loginForm.classList.add("hidden")
-    registerForm.classList.remove("hidden")
+    loginContainer.classList.add("hidden")
+    registerContainer.classList.remove("hidden")
 })
 
-goLogin.addEventListener("click", function(e) {
+goLogin.addEventListener("click", function(e) { //esta funcion hace que al hacer click en iniciar sesion cambie al form de login
     e.preventDefault()
-    registerForm.classList.add("hidden")
-    loginForm.classList.remove("hidden")
+    registerContainer.classList.add("hidden")
+    loginContainer.classList.remove("hidden")
 })
 
+
+
+// esta funcion creara un nuevo usuario en la pagina
+
+const registerForm = document.getElementById("register-form")
+
+registerForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    let usuarioReg = document.getElementById("usuario-register").value
+    let emailReg = document.getElementById("email-register").value
+    let passwordReg = document.getElementById("password-register").value
+    
+    let usuario = localStorage.getItem("usuario")
+
+    if (usuario === null) {
+        usuario = []
+    } else {
+        usuario = JSON.parse(usuario)
+    }
+
+    let usuarioExiste = usuario.find(u => u.email === emailReg)
+
+    if (usuarioExiste){
+        alert("Usuario ya existente")
+        return
+    }
+
+    const nuevoUsuario = {
+        usuario: usuarioReg,
+        email: emailReg,
+        password: passwordReg
+    }
+
+    usuario.push(nuevoUsuario)
+
+    localStorage.setItem("usuario", JSON.stringify(usuario))
+
+    alert("Usuario registrado con exito")
+})
